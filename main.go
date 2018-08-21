15a5e438a9df819595f6ab2a096b105ef646b43a
CreativeGuy2013 <CreativeGuy2013@users.noreply.github.com>
2018-08-21 15:39:14 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
+	r := mux.NewRouter()
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("")))
+
+	fmt.Println("Hosting on http://localhost:8080")
+	fmt.Println("Stop by pressing ^C")
+	fmt.Println(http.ListenAndServe(":8080", r))
 }
